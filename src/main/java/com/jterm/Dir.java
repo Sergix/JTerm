dbe38d1591348218be84675573e9afdfb19eae1f
Sergix <sergix@outlook.com>
2017-05-23 08:25:45 -0400

    Create Dir.java
    View master changelog for details.

---
+++ src/main/java/com/jterm/Dir.java
@@ -0,0 +1,23 @@
+package main.java.com.jterm;
+
+import java.io.*;
+
+public class Dir {
+
+	public static void PrintDir() {
+  
+    // Copied this snippet of code off of the java docs :P
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+    
+       for (Path file: stream) {
+          System.out.println(file.getFileName());
+       }
+       
+    } catch (IOException | DirectoryIteratorException x) {
+
+      System.err.println(x);
+      
+    }
+	}
+
+}
