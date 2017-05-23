fbe3c0754fcda3e19ee12faaca9529b7117f4c6b
Sergix <sergix@outlook.com>
2017-05-23 08:53:58 -0400

    Update Dir.java

--- src/main/java/com/jterm/Dir.java
+++ src/main/java/com/jterm/Dir.java
@@ -1,24 +1,26 @@
 package main.java.com.jterm;
 
 import java.io.*;
-import java.nio.file;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Dir {
 
-	public static void PrintDir() {
+  public static void PrintDir() {
   
     // Copied this snippet of code off of the java docs :P
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
     
-       for (Path file: stream) {
-          System.out.println(file.getFileName());
-       }
+      for (Path file: stream) {
+        System.out.println(file.getFileName());
+      }
        
     } catch (IOException | DirectoryIteratorException x) {
 
       System.err.println(x);
       
     }
-	}
-
+  }
 }
