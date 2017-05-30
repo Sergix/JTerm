d8081aba256cdfa03527d98ef027d7447ef8f8d6
Sergix <sergix@outlook.com>
2017-05-29 21:07:27 -0400

    Update v0.2.1
    View changelog for details.

--- src/main/java/com/jterm/Echo.java
+++ src/main/java/com/jterm/Echo.java
@@ -4,29 +4,37 @@ import java.util.ArrayList;
 
 public class Echo {
 	
-	
+	/*
+	* EchoInput() void
+	* 
+	* Echoes the input recieved to the console.
+	* 
+	* ArrayList<String> options - command options
+	*
+	* -h
+	*     Prints help information.
+	*/
 	public static void EchoInput(ArrayList<String> options) {
 		
 		String output = "";
 		
 		for (String option: options) {
-			
-			  if (option.equals("-h"))
-			  {
-				  System.out.println("");
-				  return;
-				  
-			  }
-			  else
-			  {
-				  output += option + " ";
-				  
-			  }
-			 
-		  }
+			if (option.equals("-h"))
+			{
+				System.out.println("Command syntax:\n\techo [-h] input\n\nPrints the specified input to the console.");
+				return;
+			  
+			}
+			else
+			{
+				output += option + " ";
+			  
+			}
+		 
+		}
+		
 		System.out.println(output);
 		
 	}
-	
 	
 }
