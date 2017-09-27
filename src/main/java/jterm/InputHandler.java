cf7cce2730db1512068201cbcf38b060552f7d79
nanoandrew4 <nanoandrew4@gmail.com>
2017-09-27 15:57:13 -0300

    Fixed issues and improved readability and ease of use with autocomplete. Tab rotation and command autocomplete still not implemented, this is more of a bug fix

--- src/main/java/jterm/InputHandler.java
+++ src/main/java/jterm/InputHandler.java
@@ -49,14 +49,17 @@ public class InputHandler {
 		} else if (input == '\t' && JTerm.command.length() > 0) { // tab
 			String[] commandArr = JTerm.command.split(" "); // split into sections
 			String currText = commandArr[commandArr.length - 1]; // get last element
-			if (commandArr.length > 1) // if more than one element, autocomplete file
+			if (commandArr.length > 1 && !JTerm.command.endsWith(" ")) // if more than one element, autocomplete file
 				FileAutocomplete(currText);
-			else if (commandArr.length == 1) // if one element, autocomplete command (to be implemented)
+			else if (commandArr.length == 1 && !JTerm.command.endsWith(" ")) // if one element, autocomplete command (to be implemented)
 				CommandAutocomplete(currText);
+			else if (JTerm.command.endsWith(" ")) // if last input was space, print all options
+				FileAutocomplete(" ");
 		} else if (input == '\n') { // enter, or new line
 			if (JTerm.command.length() > 0)
 				JTerm.Parse(JTerm.command);
 			JTerm.command = "";
+			System.out.println();
 		} else if (Character.isLetter(input)) { // its a letter
 			if (!JTerm.capsOn)
 				JTerm.command += input;
@@ -88,15 +91,18 @@ public class InputHandler {
 		} else if (input == 9 && JTerm.command.length() > 0) { // tab
 			String[] commandArr = JTerm.command.split(" "); // split into sections
 			String currText = commandArr[commandArr.length - 1]; // get last element
-			if (commandArr.length > 1) // if more than one element, autocomplete file
+			if (commandArr.length > 1 && !JTerm.command.endsWith(" ")) // if more than one element, autocomplete file
 				FileAutocomplete(currText);
-			else if (commandArr.length == 1) // if one element, autocomplete command (to be implemented)
+			else if (commandArr.length == 1 && !JTerm.command.endsWith(" ")) // if one element, autocomplete command (to be implemented)
 				CommandAutocomplete(currText);
+			else if (JTerm.command.endsWith(" ")) // if last input was space, print all options
+				FileAutocomplete(" ");
 		} else if (input == 13) { // new line
 			System.out.println("\r\n");
 			if (JTerm.command.length() > 0)
 				JTerm.Parse(JTerm.command);
 			JTerm.command = "";
+			System.out.println("\r\n");
 		} else if (Character.isLetter(input)) { // its a letter
 			if (!JTerm.capsOn)
 				JTerm.command += input;
@@ -124,20 +130,47 @@ public class InputHandler {
 		for (File f : files)
 			if (f.getName().startsWith(currText))
 				fileNames.add(f.getName());
-		// print all file names that match
+
 		if (fileNames.size() != 1) {
+			if (fileNames.size() > 0 || currText.equals(" "))
+				ClearLine(JTerm.command); // clear line
+
+			// print matching file names
 			for (String s : fileNames)
 				System.out.print(s + "\t");
+			if (fileNames.size() > 0) {
+				System.out.println();
+				System.out.print(JTerm.command); // re-output command after clearing lines
+			}
+
 			// if no input, just output all files and folders
-			if (fileNames.size() == 0)
+			if (currText.equals(" ")) {
 				for (File f : files)
 					System.out.print(f.getName() + " \t");
-			System.out.println();
+				System.out.println("\n"); // improve readability
+				System.out.print(JTerm.command); // re-output command after clearing lines
+			}
 		} else {
 			String fileName = fileNames.getFirst();
 			JTerm.command += fileName.substring(currText.length(), fileName.length()) + " ";
 			System.out.print(fileName.substring(currText.length(), fileName.length()) + " ");
+			System.out.println(); // improve readability
 		}
+	}
+
+	/**
+	 * ClearLine()
+	 *
+	 * Clears a line in the console of size line.length()
+	 * @param line line to be cleared
+	 */
+	private static void ClearLine(String line) {
+		for (int i = 0; i < line.length(); i++)
+			System.out.print("\b");
+		for (int i = 0; i < line.length(); i++)
+			System.out.print(" ");
+		for (int i = 0; i < line.length(); i++)
+			System.out.print("\b");
 	}
 
 	/**
