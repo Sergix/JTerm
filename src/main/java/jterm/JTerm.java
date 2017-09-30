4ec17da8a152092542e4e66124b26ba8dc700a07
Cody Metz <ccmetz92@gmail.com>
2017-09-29 19:46:39 -0500

    Handle all Unix operating systems

--- src/main/java/jterm/JTerm.java
+++ src/main/java/jterm/JTerm.java
@@ -17,6 +17,8 @@
 // package = folder :P
 package main.java.jterm;
 
+import org.apache.commons.lang3.SystemUtils;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.lang.reflect.Constructor;
@@ -35,8 +37,8 @@ public class JTerm
 	// Default value "./" is equal to the default directory set when the program starts
 	static String currentDirectory = "./";
 	static String os = System.getProperty("os.name").toLowerCase();
-	static boolean isWin = os.contains("windows");
-	static boolean isUnix = os.equals("linux");
+	static boolean isWin = SystemUtils.IS_OS_WINDOWS;
+	static boolean isUnix = SystemUtils.IS_OS_UNIX;
 
 	// User input variable used among all parts of the application
 	static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
