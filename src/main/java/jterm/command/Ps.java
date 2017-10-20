2b27127e98602a4f749e67cebf2d8ad4b021c63a
Oles Pankiv <ojlesua@gmail.com>
2017-10-20 18:43:14 +0300

    command/Ps.java  * formatted code  * used try-with-resources

--- src/main/java/jterm/command/Ps.java
+++ src/main/java/jterm/command/Ps.java
@@ -1,92 +1,62 @@
 package jterm.command;
 
+import org.apache.commons.lang3.SystemUtils;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
-import org.apache.commons.lang3.SystemUtils;
 
 /*
 * Original code credit to @chromechris
 * 
 * (edits for release done by @Sergix)
 */
-public class Ps 
-{
+public class Ps {
+    /*
+    * Ps() void
+    *
+    * Prints a list of process running on
+    * the system.
+    *
+    * ArrayList<String> options - command options
+    *
+    * -h
+    * 	Prints help information
+    */
+    public Ps(ArrayList<String> options) {
+        if (options.contains("-h")) {
+            System.out.println("Command syntax:\n\tps [-h]\n\nDisplays all current processes running on the host system.");
+            return;
+        }
 
-	/*
-	* Ps() void
-	* 
-	* Prints a list of process running on
-	* the system.
-	*
-	* ArrayList<String> options - command options
-	*
-	* -h
-	* 	Prints help information
-	*/
-	public Ps(ArrayList<String> options)
-	{
+        // FIXME: use SystemUtils or JTerm.IS_UNIX constant?
+        if (SystemUtils.IS_OS_LINUX) {
+            try {
+                Process process = Runtime.getRuntime().exec("ps -e");
+                BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-		for (String option: options)
-		{
-			if (option.equals("-h"))
-			{
-				System.out.println("Command syntax:\n\tps [-h]\n\nDisplays all current processes running on the host system.");
-				return;
-				
-			}
-			
-		}
-
-		if (SystemUtils.IS_OS_LINUX)
-		{
-			try
-			{
-				String line;
-				Process p = Runtime.getRuntime().exec("ps -e");
-				BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-				while ((line = input.readLine()) != null)
-				{
-					// Parse data here.
-					System.out.println(line);
-					
-				}
-				input.close();
-				
-			}
-			catch (Exception err)
-			{
-				err.printStackTrace();
-				
-			}
-
-		}
-		else if (SystemUtils.IS_OS_WINDOWS)
-		{
-			try
-			{
-				String line;
-				Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe");
-				BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-				while ((line = input.readLine()) != null)
-				{
-					// Parse data here.
-					System.out.println(line);
-					
-				}
-				
-				input.close();
-				
-			}
-			catch (Exception err)
-			{
-				err.printStackTrace();
-				
-			}
-			
-		}
-
-	}
-
+                // TODO: can be replaced with IOUtils.toString()  (from apache commons)
+                String line;
+                while ((line = input.readLine()) != null) {
+                    System.out.println(line);
+                }
+                input.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (SystemUtils.IS_OS_WINDOWS) {
+            try {
+                String line;
+                Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe");
+                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                while ((line = input.readLine()) != null) {
+                    // Parse data here.
+                    System.out.println(line);
+                }
+                input.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
