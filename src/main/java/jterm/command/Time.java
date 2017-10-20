c282cccf2ae0826fd413794ad59841a911c35680
Oles Pankiv <ojlesua@gmail.com>
2017-10-20 16:33:45 +0300

    command/Time.java  * formatted code  * replaced for loop with opsiont.contains()

--- src/main/java/jterm/command/Time.java
+++ src/main/java/jterm/command/Time.java
@@ -1,32 +1,18 @@
 package jterm.command;
 
-import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Date;
 
 public class Time {
-	
-    
-	public Time(ArrayList<String> options)
-	{
-		
-		for (String option: options)
-		{
-			if (option.equals("-h"))
-			{
-				System.out.println("Command syntax:\n\techo [-h] input\n\nPrints the specified input to the console."); // Options
-				return;
-			  
-			}
-		 
-		}
-		
-		DateFormat df = new SimpleDateFormat("HH:mm:ss , z");
-	    Date dateobj = new Date();
-	    System.out.println("The Current Time is: " + df.format(dateobj));
-		
-	}
-	
+    public Time(ArrayList<String> options) {
+        if (options.contains("-h")) {
+            System.out.println("Command syntax:\n\techo [-h] input\n\nPrints the specified input to the console."); // Options
+            return;
+        }
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss , z");
+        System.out.println("The Current Time is: " + dateFormat.format(date));
+    }
 }
