e16a3e84633d60eae93834719c95e9e7471b8573
Oles Pankiv <ojlesua@gmail.com>
2017-10-20 16:12:01 +0300

    command/Pause.java  * formatted code

--- src/main/java/jterm/command/Pause.java
+++ src/main/java/jterm/command/Pause.java
@@ -5,40 +5,30 @@ import java.io.IOException;
 
 import jterm.JTerm;
 
-public class Pause
-{
+public class Pause {
+    /*
+    * Pause() void
+    *
+    * Pauses the interpreter until the user
+    * hits the "Enter" key.
+    *
+    * ArrayList<String> options - command options
+    *
+    * message
+    * 	Pause message to be printed
+    */
+    public Pause(ArrayList<String> options) {
+        if (options.size() == 0) {
+            System.out.print("Press enter to continue...");
+        } else {
+            System.out.print(Exec.getRest(options, 0));
+        }
 
-	/*
-	* Pause() void
-	* 
-	* Pauses the interpreter until the user
-	* hits the "Enter" key.
-	*
-	* ArrayList<String> options - command options
-	*
-	* message
-	* 	Pause message to be printed
-	*/
-	public Pause(ArrayList<String> options) { 
-
-		if (options.size() == 0)
-			System.out.print("Press enter to continue...");
-		
-		else
-			System.out.print(Exec.GetRest(options, 0));
-
-		try
-		{
-			JTerm.userInput.read();
-			JTerm.userInput.skip(1);
-			
-		}
-		catch (IOException ioe)
-		{
-			System.out.println(ioe);
-
-		}
-
-	}
-
+        try {
+            JTerm.userInput.read();
+            JTerm.userInput.skip(1);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
