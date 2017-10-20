a68727e81a3a52e106974bdf8004f186cfe87926
Oles Pankiv <ojlesua@gmail.com>
2017-10-20 18:03:41 +0300

    command/Date.java  * formatted code  * removed for loop changed java version to 1.8, because LocalDate(used in command/Date.java) is since 1.8

--- src/main/java/jterm/command/Date.java
+++ src/main/java/jterm/command/Date.java
@@ -4,25 +4,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Date {
-	
-    
-	public Date(ArrayList<String> options)
-	{
-		
-		for (String option: options)
-		{
-			if (option.equals("-h"))
-			{
-				System.out.println("Command syntax:\n\techo [-h] input\n\nPrints the specified input to the console."); // Options
-				return;
-			  
-			}
-		 
-		}
-		
-		LocalDate today = LocalDate.now();
-		System.out.println("The Current Date is " + today + " (yyyy-mm-dd)");
-		
-	}
-	
+    public Date(ArrayList<String> options) {
+        if (options.contains("-h")) {
+            System.out.println("Command syntax:\n\techo [-h] input\n\nPrints the specified input to the console."); // Options
+            return;
+        }
+        System.out.println("The Current Date is " + LocalDate.now() + " (yyyy-mm-dd)");
+    }
 }
