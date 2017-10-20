e51d33f47dc8f8e3c4ec7e590e2686972b57c722
Oles Pankiv <ojlesua@gmail.com>
2017-10-20 15:27:07 +0300

    Refactored command/Clean.java (formatted code and replaced for loop with ontions.contains())

--- src/main/java/jterm/command/Clear.java
+++ src/main/java/jterm/command/Clear.java
@@ -5,43 +5,23 @@ import jterm.JTerm;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Class for the "Clear" command
- * <p>
- * Clears the terminal display by removing all lines
- */
-public class Clear
-{
-    final private String ANSI_CLS = "\u001b[2J";
-    final private String ANSI_HOME = "\u001b[H";
+public class Clear {
+    private static final String ANSI_CLS = "\u001b[2J";
+    private static final String ANSI_HOME = "\u001b[H";
 
-    public Clear(ArrayList<String> options)
-    {
-        for (String option : options)
-        {
-            if (option.equals("-h"))
-            {
-                System.out.println("Command syntax:\n\tclear [-h]\n\nClears all lines in the terminal display.");
-            }
-        }
-
-        // If '-h' was not used - Clear the Screen
-        if (!options.contains("-h"))
-        {
-            if (JTerm.isUnix)
-            {
+    public Clear(ArrayList<String> options) {
+        if (options.contains("-h")) {
+            System.out.println("Command syntax:\n\tclear [-h]\n\nClears all lines in the terminal display.");
+        } else {
+            if (JTerm.IS_UNIX) {
                 // Use escape sequences to clear the screen for Unix OS
                 System.out.print(ANSI_CLS + ANSI_HOME);
                 System.out.flush();
-            }
-            else if (JTerm.isWin)
-            {
+            } else if (JTerm.IS_WIN) {
                 // Invoke the command line interpreter's own 'clear' command for Windows OS
-                try
-                {
+                try {
                     new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-                } catch (IOException | InterruptedException e)
-                {
+                } catch (IOException | InterruptedException e) {
                     System.out.println(e);
                 }
             }
