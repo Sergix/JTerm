58b9d341d4df05e97a33548cd3a99a5ba9db7203
larperdoodle <larsbenedetto@gmail.com>
2017-11-22 17:20:57 -0800

    Added regex command

---
+++ src/main/java/jterm/command/Regex.java
@@ -0,0 +1,56 @@
+package jterm.command;
+
+import jterm.JTerm;
+import jterm.util.PrintStreamCollector;
+import jterm.util.PromptPrinter;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    private static final Pattern regex = Pattern.compile("\"(.*)\"");
+
+    @Command(name = {"regex", "grep"}, syntax = "regex [-m] \"regex\" command")
+    public static void clearScreen(List<String> options) {
+        PromptPrinter backup = JTerm.out;
+        StringBuilder commandBuilder = new StringBuilder();
+        boolean multiline = options.get(0).equals("-g");
+        if (multiline) options.remove(0);
+        options.forEach(commandBuilder::append);
+        String command = commandBuilder.toString();
+        Matcher m = regex.matcher(command);
+        String exp;
+        if (m.find()) {
+            exp = m.group(1);
+            command = command.replaceFirst("\".*\"", "");
+            PrintStreamCollector collector = new PrintStreamCollector();
+            JTerm.out = collector;
+            JTerm.executeCommand(command);
+            Pattern pattern = Pattern.compile(exp);
+            boolean found = false;
+            if (multiline) {
+                String s = collector.export();
+                Matcher matcher = pattern.matcher(s);
+                while (matcher.find()) {
+                    found = true;
+                    for (int i = 1; i <= matcher.groupCount(); i++)
+                        backup.println(matcher.group(i));
+                }
+            } else {
+                String[] s = collector.exportArray();
+                for (String line : s) {
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        backup.println(line);
+                        found = true;
+                    }
+                }
+            }
+            if (!found) backup.println("No matches");
+        } else {
+            backup.println("Invalid syntax");
+        }
+        JTerm.out = backup;
+    }
+}
