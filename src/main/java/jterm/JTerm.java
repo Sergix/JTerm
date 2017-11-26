b0df249bc5e2df37401e62fbdccbeee5386a00e0
Sergix <sergix@outlook.com>
2017-11-25 19:30:40 -0500

    Command Initialization Fix Vew changelog entry #120 for details.

--- src/main/java/jterm/JTerm.java
+++ src/main/java/jterm/JTerm.java
@@ -118,31 +118,45 @@ public class JTerm {
     }
 
     private static void initCommands() {
-        // Reflections reflections = new Reflections("jterm.command", new MethodAnnotationsScanner());
-        // Set<Method> methods = reflections.getMethodsAnnotatedWith(Command.class);
         ArrayList<Method> methods = new ArrayList<>();
+        ArrayList<String> classes = new ArrayList<>();
 
         try {
             CodeSource src = JTerm.class.getProtectionDomain().getCodeSource();
             if (src != null) {
                 ZipInputStream zip = new ZipInputStream(src.getLocation().openStream());
 
-                ZipEntry e;
-                while ((e = zip.getNextEntry()) != null) {
+                while (true) {
+                    ZipEntry e = zip.getNextEntry();
+
+                    if (e == null) {
+                        break;
+                    }
+
                     String name = e.getName();
                     if (name.startsWith("jterm/command")) {
-                        Class clazz = Class.forName(name.replace('/', '.').substring(0, name.length() - 6));
-                        if (!clazz.getSimpleName().startsWith("Command")) {
-                            Arrays.stream(clazz.getDeclaredMethods())
-                                    .filter(method -> method.isAnnotationPresent(Command.class))
-                                    .forEach(methods::add);
-                        }
+                        classes.add(name.replace('/', '.').substring(0, name.length() - 6));
                     }
                 }
             }
-        } catch (IOException | ClassNotFoundException e) {
-            out.println(TextColor.ERROR, e.toString());
+        } catch (IOException ioe) {
+            out.println(TextColor.ERROR, ioe.toString());
         }
+
+        // TODO: This line makes the program crash on Linux Kubuntu, don't know about windows
+        classes.remove(0);
+
+        classes.forEach(aClass -> {
+            try {
+                Arrays.stream(Class.forName(aClass).getDeclaredMethods()).forEach(method -> {
+                    if (method.isAnnotationPresent(Command.class)) {
+                        methods.add(method);
+                    }
+                });
+            } catch (ClassNotFoundException cnfe) {
+                out.println(TextColor.ERROR,cnfe.toString());
+            }
+        });
 
         methods.forEach(method -> {
             method.setAccessible(true);
